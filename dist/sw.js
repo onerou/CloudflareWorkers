c105fa99507210356eac552de33bd84c33920bff
name <1057674027@qq.com>
2022-04-03 23:45:22 +0800

    change

---
+++ dist/sw.js
@@ -0,0 +1,9 @@
+// import { registerRoute } from "workbox-routing";
+// import { StaleWhileRevalidate } from "workbox-strategies";
+
+// registerRoute(
+//   ({ request }) =>
+//     request.destination === "script" || request.destination === "style",
+//   new StaleWhileRevalidate()
+// );
+console.log("Hello from service-worker.js");
