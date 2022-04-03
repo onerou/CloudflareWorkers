51537922a2877fc05a80feb27c8d0e3d752218cf
name <1057674027@qq.com>
2022-04-03 23:25:33 +0800

    change

--- build.js
+++ build.js
@@ -6,14 +6,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 try {
-  await build({
+  build({
     bundle: true,
     sourcemap: true,
     format: "esm",
     target: "esnext",
     entryPoints: [path.join(__dirname, "src", "index.ts")],
     outdir: path.join(__dirname, "dist"),
-    outExtension: { ".js": ".mjs" },
+    outExtension: { ".js": ".mjs" }
   });
 } catch {
   process.exitCode = 1;
