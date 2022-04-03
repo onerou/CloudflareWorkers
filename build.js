3bf010207b13392d0771df0b4b82df64a4a21600
name <1057674027@qq.com>
2022-04-03 22:47:58 +0800

    improve：change

--- build.js
+++ build.js
@@ -1,13 +1,17 @@
-const path = require("path");
-const { build } = require("esbuild");
+import path from "path";
+import { fileURLToPath } from "url";
+import { build } from "esbuild";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 try {
-  build({
+  await build({
     bundle: true,
     sourcemap: true,
     format: "esm",
     target: "esnext",
-    entryPoints: [path.join(__dirname, "src", "index.tsx")],
+    entryPoints: [path.join(__dirname, "src", "index.ts")],
     outdir: path.join(__dirname, "dist"),
     outExtension: { ".js": ".mjs" },
   });
