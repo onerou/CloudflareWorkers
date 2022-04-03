b7fb0078b3124a3faf501946033d7f89d7efa413
name <1057674027@qq.com>
2022-04-03 21:43:10 +0800

    add

--- build.js
+++ build.js
@@ -1,16 +1,16 @@
 const path = require("path");
 const { build } = require("esbuild");
 
-
 try {
   build({
     bundle: true,
     sourcemap: true,
     format: "esm",
     target: "esnext",
-    entryPoints: [path.join(__dirname, "src", "index.jsx")],
+    entryPoints: [path.join(__dirname, "src", "index.tsx")],
     outdir: path.join(__dirname, "dist"),
-    outExtension: { ".js": ".mjs" }
+    outExtension: { ".js": ".mjs" },
+    include: [path.join(__dirname, "src", "sw.ts")]
   });
 } catch {
   process.exitCode = 1;
