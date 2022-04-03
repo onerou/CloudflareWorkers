61b86a5e2cb0321bff803fbeec8adc96c4fdc81d
name <1057674027@qq.com>
2022-04-04 00:04:20 +0800

    changeSW

--- src/sw.ts
+++ src/sw.ts
@@ -1,9 +1,15 @@
-import { registerRoute } from "workbox-routing";
-import { StaleWhileRevalidate } from "workbox-strategies";
+import { precacheAndRoute, matchPrecache } from "workbox-precaching";
+import { setCatchHandler } from "workbox-routing";
 
-registerRoute(
-  ({ request }: any) =>
-    request.destination === "script" || request.destination === "style",
-  new StaleWhileRevalidate()
-);
-console.log("Hello from service-worker.js");
+// Ensure your build step is configured to include /offline.html as part of your precache manifest.
+precacheAndRoute(self.__WB_MANIFEST);
+
+// Catch routing errors, like if the user is offline
+setCatchHandler(async ({ event }) => {
+  // Return the precached offline page if a document is being requested
+  if (event.request.destination === "document") {
+    // return matchPrecache("/offline.html");
+  }
+
+  return Response.error();
+});
