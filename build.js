1fa996c6e7a1adcd65d71728a6844b76128e1b0e
name <1057674027@qq.com>
2022-04-03 23:37:21 +0800

    change

--- build.js
+++ build.js
@@ -1,12 +1,12 @@
 import path from "path";
 import { fileURLToPath } from "url";
-import { build } from "esbuild";
+import build from "esbuild";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 try {
-  build({
+  build.build({
     bundle: true,
     sourcemap: true,
     format: "esm",
